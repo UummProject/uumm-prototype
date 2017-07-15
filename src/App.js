10722ffb7ac5c5106a3c0181ece2a68e63372793
Xavi <xavings@gmail.com>
2017-07-15 23:53:33 +0200

    Cleaning up nested promises

--- src/App.js
+++ src/App.js
@@ -40,70 +40,43 @@ class App extends Component {
      */
 
     // So we can update state later.
-    var self = this
-/*
-    // Get the RPC provider and setup our SimpleStorage contract.
-    var {host, port} = Config.networks[process.env.NODE_ENV]
-    
-    const provider = new Web3.providers.HttpProvider('http://' + host + ':' + port)
-    const contract = require('truffle-contract')
-    const uummContract = contract(UummContract)
-    uummContract.setProvider(provider)
-
-    // Get Web3 so we can get our accounts.
-    const web3RPC = new Web3(provider)
-
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var uummContractInstance
-
-    // Get accounts.
-    web3RPC.eth.getAccounts(function(error, accounts) {
-      console.log(accounts)
-
-      uummContract.deployed().then(function(instance) {
-        uummContractInstance = instance
-
-        // Stores a value of 5.
-        return uummContractInstance.set(5, {from: accounts[0]})
-      }).then(function(result) {
-        // Get the value from the contract to prove it worked.
-        return uummContractInstance.get.call(accounts[0])
-      }).then(function(result) {
-        // Update state with the result.
-        return self.setState({ storageValue: result.c[0] })
-      })
-    })
-    */
+   
   }
 
     onNewProject()
     {
         var {host, port} = Config.networks[process.env.NODE_ENV]
-        const web3RPC = new Web3(provider)
+        
         const provider = new Web3.providers.HttpProvider('http://' + host + ':' + port)
+        
+
         const contract = require('truffle-contract')
         const uummContract = contract(UummContract)
+        uummContract.setProvider(provider)
         var uummContractInstance
+        var estimateGas
 
+        const web3RPC = new Web3(provider)
         web3RPC.eth.getAccounts(function(error, accounts)
         {
             console.log(accounts)
             
-            uummContract.deployed().then(
-                function(instance)
-                {
-                    uummContractInstance = instance
+            uummContract.deployed()
+            .then(function(instance){
+                uummContractInstance = instance;
+                return uummContractInstance.CreateProject.estimateGas()
+            }).then(function(gas){
+                
+                estimateGas = gas
+                console.log(estimateGas===280481);
+                return uummContractInstance.CreateProject("Ultimate unicorn maker machine", {from: accounts[0], gas:280481})
+            }).then(function(result) {
+                return uummContractInstance.GetProjectsLength.call(accounts[0])
+            }).then(function(result) {
+                console.log(result);
+            })
 
-                    return uummContractInstance.createProject("Ultimate unicorn maker machine", {from: accounts[0]})
-                }).then(
-                function(result) {
-                    return uummContractInstance.GetProjectsLength.call(accounts[0])
-                }).then(function(result) {
-                    console.log(result);
-                })
-
-        })
-        
+        }) 
     }
 
   render() {
