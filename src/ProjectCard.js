e05504abb0bad5649545721cc195f992c1eb81db
Xavi <xavings@gmail.com>
2017-07-16 14:36:02 +0200

    Basic ProjectCard component to display existing projects in a list

---
+++ src/ProjectCard.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const containerStyle =
+{
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height : 80,
+    padding : 15,
+    marginTop : 10,
+    margnBottom : 30,
+    marginLeft:5,
+    marginRight:5
+}
+
+const titleStyle =
+{
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flexStart',
+    flexGrow: 4
+}
+
+
+class CommitCard extends React.Component {
+
+    constructor(props) {
+        super();
+    }
+
+    render()
+    {
+        return (
+            <Paper style={containerStyle} zDepth={1} >
+
+                <h3 style = {titleStyle}> {this.props.title} </h3>
+                
+            </Paper>
+        );
+    }
+}
+
+export default CommitCard;
