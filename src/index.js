37ac05b5292b89d0d348978a085123450e3c3f94
Xavi <xavings@gmail.com>
2017-07-16 19:21:29 +0200

    Unused module.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import truffleConfig from './../truffle-config.js'
+//import truffleConfig from './../truffle-config.js'
 
 ReactDOM.render(
   <App />,
