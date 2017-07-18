0a7fd7dd8a350e9dc49974a2ccc88b4a71d3cbe7
Xavi <xavings@gmail.com>
2017-07-18 23:38:03 +0200

    Updating projectDetails with new state management

--- src/ProjectDetails.js
+++ src/ProjectDetails.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Paper from 'material-ui/Paper';
 import Avatar from 'material-ui/Avatar';
-import FileFolder from 'material-ui/svg-icons/file/folder';
 import FontIcon from 'material-ui/FontIcon';
+import State from './State.js'
+import Uumm from './UummContractInterface.js'
 
 import {
   blue300,
@@ -46,19 +46,39 @@ class ProjectDetails extends React.Component {
         super();
     }
 
+    componentWillMount=()=>
+    {
+        /*var that = this
+        Uumm.isReady()
+        .then(function(){
+            Uumm.getUserProjects()
+            .then(function(projects){
+                that.setState({'projects':projects})
+            }).catch(function(error){console.log(error)})
+        }).catch(function(error){console.log(error)}) 
+        */
+
+        Uumm.isReady()
+        .then(Uumm.getProjectDetails(this.props.projectId))
+    }
+
     render()
     {
+        var projectDetails = State.projects[this.props.projectId]
+        var firstCarachter = State
+
         return (
-            <Paper  zDepth={1} >
-            <Avatar
-                color={deepOrange300}
-                backgroundColor={purple500}
-                size={30}
-                style={style}>
-                A
-            </Avatar>
-                <h4 style={titleStyle}> Unga </h4>       
-            </Paper>
+            <div >
+                <Avatar
+                    color={deepOrange300}
+                    backgroundColor={purple500}
+                    size={30}
+                    style={style}>
+                    {projectDetails.name[0]}
+                </Avatar>
+                    <h4 style={titleStyle}> {projectDetails.name} </h4> 
+                    <p style={titleStyle}> {projectDetails.id} </p>       
+            </div>
         );
     }
 }
