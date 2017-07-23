2b8748cc6942b0b78035b79d614e954e26094966
Xavi <xavings@gmail.com>
2017-07-23 14:27:46 +0200

    Adding gas limit to truffle config. Adding production network

--- truffle-config.js
+++ truffle-config.js
@@ -9,7 +9,15 @@ module.exports = {
     ropsten: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      gas: 4612388,
+      network_id: "3"
+    },
+    //When "npm start build",  process.env.NODE_ENV == production
+    production: {
+      host: "localhost",
+      port: 8545,
+      gas: 4612388,
+      network_id: "3"
     }
   }
 };
