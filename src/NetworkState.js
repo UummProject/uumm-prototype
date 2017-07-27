b823cdea2c95ec013380a89bdb2538634c4101bc
Xavi <xavings@gmail.com>
2017-07-27 20:53:04 +0200

    Adding hints to diagnose connection.

--- src/NetworkState.js
+++ src/NetworkState.js
@@ -2,21 +2,23 @@ import React from 'react'
 import Paper from 'material-ui/Paper'
 import Uumm from './UummContractInterface.js'
 import Web3AutoSetup from './Web3AutoSetup.js'
+import Video from 'react-youtube'
 
 const containerStyle =
 {
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'nowrap',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
-    height : 80,
     padding : 15,
-    marginTop : 10,
+    marginTop : 0,
     margnBottom : 30,
-    marginLeft:5,
-    marginRight:5
+    marginLeft:0,
+    marginRight:0
 }
+
+const metamaskVideoId = "6Gf_kRE4MJU"
 
 class NetworkState extends React.Component {
 
@@ -28,7 +30,7 @@ class NetworkState extends React.Component {
             "connected" : false,
             "network" : "",
             "provider":"Not found",
-            "userAddress":"-"
+            "userAddress":""
         }
 
         Uumm.isReady()
@@ -47,7 +49,6 @@ class NetworkState extends React.Component {
 
         Web3AutoSetup.addAccountChangedListener(this.onAddressChange)
         Web3AutoSetup.addNetworkChangedListener(this.onNetworkChange)
-
     }
 
     onAddressChange=(newAddress)=>
@@ -58,22 +59,70 @@ class NetworkState extends React.Component {
 
     onNetworkChange=(newNetworkId)=>
     {
-        this.setState({ "provider":Web3AutoSetup.getCurrentNetwork().name})
+        this.setState({ "network":Web3AutoSetup.getCurrentNetwork().name})
     }
 
     render()
     { 
+        var hint=undefined
+        var hint1=undefined
+        var video=undefined
+        if(this.state.provider === "MetaMask" && !this.state.userAddress)
+        {
+            hint= "Unlock MetaMask to interact with the contract"
+        }
+
+        if(this.state.network !== "Ropsten")
+            hint= "The contract is only deployed on the Ropsten Network. Make sure you are on the right network"
+
+        if(!this.state.connected)
+        {
+
+            hint= "In order to access the contract we need a connection to Ethereum network."
+            hint1 = "The easiest way is to install Metamaks"
+            video =<Video videoId="6Gf_kRE4MJU"/>
+        }
+
+
         var connected = "false"
         if(this.state.connected)
             connected = "true"
+
+        var hintContainer=<div/>
+
+        var installMetamaskContainer=
+            <div>
+                <p> {hint}</p>
+                <a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn"> {"Install MetaMask"}</a>
+                {video}
+            </div>
+
+
+
+        if(hint)
+            hintContainer = (<Paper style={containerStyle} >
+                                <div>
+                                    <p> {hint}</p>
+                                </div>
+                            </Paper>)
+
+        
+
         return (
-            <Paper style={containerStyle} zDepth={0} >
-                <p> Connected: {connected} </p> 
-                <p> Network: {this.state.network} </p> 
-                <p> Provider: {this.state.provider} </p>
-                <p> Your address: {this.state.userAddress}</p>
+            <div>
+                <Paper style={containerStyle} >
+                        <p> Connected: {connected} </p> 
+                        <p> Network: {this.state.network} </p> 
+                        <p> Provider: {this.state.provider} </p>
+                        <p> Your address: {this.state.userAddress}</p>
                 
-            </Paper>
+                </Paper>
+
+               {hintContainer}
+
+
+
+            </div>
         )
     }
 }
