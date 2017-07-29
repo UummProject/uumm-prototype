b6c2a77c2aaadb4b5d9ddc904b1cdcdce610da24
Xavi <xavings@gmail.com>
2017-07-29 15:06:42 +0200

    Playing with background coloring

--- src/GreyTheme.js
+++ src/GreyTheme.js
@@ -3,19 +3,24 @@ import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import * as Colors from 'material-ui/styles/colors';
 import { fade } from 'material-ui/utils/colorManipulator'
 
+//using https://cimdalli.github.io/mui-theme-generator/
+
 const getTheme = () => {
   let overwrites = {
-    fontFamily: 'Open Sans Condensed, sans-serif',
+    "fontFamily": "Open Sans Condensed, sans-serif",
     "palette": {
-        "primary1Color": fade(Colors.black, 0.49),
-        "accent1Color": fade(Colors.darkBlack, 0.87)
+        "primary1Color": "rgba(0, 0, 0, 0.49)",
+        "accent1Color": "rgba(0, 0, 0, 0.87)",
+        "canvasColor": "#F8F7F5"
     },
     "tabs": {
-        "backgroundColor": Colors.white,
-        "textColor": fade(Colors.lightBlack, 0.54),
-        "selectedTextColor": Colors.grey800
+        "backgroundColor": "#F8F7F5",
+        "textColor": "rgba(0, 0, 0, 0.54)",
+        "selectedTextColor": "#424242"
     }
-    };
+}
+    //console.log(JSON.stringify(overwrites))
+
   return getMuiTheme(baseTheme, overwrites);
 }
 
