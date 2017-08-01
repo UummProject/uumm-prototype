7d03a9b442f7169ef0eedd419736f3806904132a
Xavi <xavings@gmail.com>
2017-08-01 16:45:42 +0200

    Displaying project details nicly

--- src/ProjectOverview.js
+++ src/ProjectOverview.js
@@ -6,12 +6,31 @@ import Numeral from 'numeral'
 import MarkdownLoader from './MarkdownLoader.js'
 import Divider from 'material-ui/Divider';
 
+const overflowStyle = {
+    whiteSpace: "nowrap",
+    textOverflow: "ellipsis",
+    overflow: "hidden"
+}
+
+const subheaderStyle = {
+    color:"grey",
+    marginBottom:-10,
+    marginTop:0
+}
+
 class ProjectDetails extends React.Component {
 
     constructor(props)
     {
         super()
         var address = process.env.PUBLIC_URL
+    }
+
+    getAddressStyle=(color)=>
+    {
+        var style = overflowStyle
+        style.color = color
+        return style
     }
 
     render()
@@ -27,33 +46,33 @@ class ProjectDetails extends React.Component {
 
         var ownership = Numeral(contributorData.valueTokens/projectData.totalSupply).format('0.0%')
 
-        var userContent = {}
-        if(contributorData.valueTokens!==0)
-        {
-            userContent= <div>          
-               <OwnershipChart userTokens={contributorData.valueTokens} totalSupply={projectData.totalSupply} />
-           </div>
-        }
-        else
-        {
-             userContent= <div> 
-                <p style={{color:"red"}}>
-                    Your account doesn't own any shares of this project, therefore you can't vote or resolve proposals.
-                </p>
-                <p style={{color:"red"}}>
-                    You can still make proposals though.
-                </p>
-            </div>
-        }
-            
-        
         return (
-           <div>    
-                <a href ={"http://localhost:3000/#projectId="+projectData.id}> Project Id: {projectData.id} </a>
-                <Divider/>      
-                {userContent}
-                <Divider/>
-                <MarkdownLoader url="https://raw.githubusercontent.com/xavivives/Uumm/master/README.md"/>
+            <div>
+
+               <div style={{display:"flex", flexDirection:"row"}}> 
+                    <OwnershipChart
+                        userTokens={contributorData.valueTokens}
+                        totalSupply={projectData.totalSupply}
+                        /> 
+
+                    <div style={{display:"flex",flexDirection:"column", overflow: "hidden",  paddingLeft:10 }}>
+                        <h5 style={subheaderStyle}>Project ID</h5>
+                        <p style={overflowStyle}> {projectData.id} </p>
+                        <h5 style={subheaderStyle}>Project total token supply</h5>
+                        <p> {projectData.totalSupply} </p>
+                        <h5 style={subheaderStyle}>Your address</h5>
+                        <p style={this.getAddressStyle("#ff3366")}> {this.props.userAddress}</p>
+                        <h5 style={subheaderStyle}>Your tokens</h5>
+                        <p> {contributorData.valueTokens} </p>
+                        <h5 style={subheaderStyle}>Available ethereum to withdraw</h5>
+                        <p> {contributorData.ethereumBalance} ETH </p>
+
+                    </div>
+                    
+                    <Divider/>
+
+
+                </div>
            </div>
         )
     }
