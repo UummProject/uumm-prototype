a5c2f57c427d41ce2c5588d4202c1314771422c3
Xavi <xavings@gmail.com>
2017-08-07 19:26:06 +0200

    Trying to assert that an error is thrown in an async call WIP

--- test/Uumm.js
+++ test/Uumm.js
@@ -4,11 +4,21 @@ const initialSateResults = {
     projectsLength : 0,
     projectId:0,
     proposalsLength:0,
-    pendingProposalsLength:0
+    pendingProposalsLength:0,
+    proposalDetails:
+    {
+        proposalId:"",
+        author:"",
+        title:0,
+        reference:0,
+        valueAmount:0,
+        creationDate:0
+    }
 }
 
 const arguments = {
-    unexistingProjectId:"32bytesString"
+    unexistingProjectId:"32bytesString",
+    unexistingProposalId:123
 }
 
 contract('Uumm', async function(accounts)
@@ -34,7 +44,24 @@ contract('Uumm', async function(accounts)
 
         //PendingProposalLength
         let pendingProposalsLength = await uummInstance.GetPendingProposalsLength.call(arguments.unexistingProjectId, {from: projectCreator});
-        assert.equal (pendingProposalsLength.toNumber(), initialSateResults.pendingProposalsLength, "No pending proposal should exist yet");    
+        assert.equal (pendingProposalsLength.toNumber(), initialSateResults.pendingProposalsLength, "No pending proposal should exist yet");
+
+        //ProposalDetails
+
+        //let proposalDetails = await uummInstance.GetProposalDetails.call(arguments.unexistingProjectId, arguments.unexistingProposalId, {from: projectCreator});
+        assert.throws ( function(){ uummInstance.GetProposalDetails.call(arguments.unexistingProjectId, arguments.unexistingProposalId, {from: projectCreator})},Error, "Blah");
+
+        //ProposalDetails - id
+        //assert.equal(proposalDetails[0], initialSateResults.proposalDetails.proposalId, "Uninitialized proposal id should be empty");
+        //ProposalDetails - author
+        //assert.equal(proposalDetails[1], initialSateResults.proposalDetails.author, "Uninitialized proposal id should be empty");
+
+        //proposalData.id = proposalDetails[0].toNumber()
+        //proposalData.author = proposalDetails[1]
+        //proposalData.title = proposalDetails[2]
+        //proposalData.reference = proposalDetails[3]
+        //proposalData.valueAmount = proposalDetails[4].toNumber()
+        //proposalData.creationDate = new Date (proposalDetails[5].toNumber()*1000)
 
     });
 
