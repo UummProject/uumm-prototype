6fe2f5566689d4f9a07490e513ba8cc06841d9c7
Xavi <xavings@gmail.com>
2017-08-10 20:38:14 +0200

    Adding testdata

--- test/TestData.js
+++ test/TestData.js
@@ -8,32 +8,39 @@ exports.proposal1=
     creationTimestamp:0,
     stateData:[
         {
-            //Initial state
+            //#0 Initial state
             state:0,
             positiveVotes:0,
             negativeVotes:0,
             totalSupply:1
         },
         {
-            //ProjectCreator (1token) voted to approve
+            //#1 ProjectCreator (1token) voted to approve
             state:0,
             positiveVotes:1,
             negativeVotes:0,
             totalSupply:1
         },
         {
-            //ProjectCreator (1token) voted to approve(again). Data stays the same
+            //#2 ProjectCreator (1token) voted to approve(again). Data stays the same
             state:0,
             positiveVotes:1,
             negativeVotes:0,
             totalSupply:1
         },
         {
-            //ProjectCreator (1token) voted against. Postive vote goes back to zero, negative incresases
+            //#3 ProjectCreator (1token) voted against. Postive vote goes back to zero, negative to one
             state:0,
             positiveVotes:0,
             negativeVotes:1,
             totalSupply:1
         },
+        {
+            //#4 ProjectCreator (1token) voted in favor. Negative vote goes back to zero, positive to  one
+            state:0,
+            positiveVotes:1,
+            negativeVotes:0,
+            totalSupply:1
+        }
     ]
 }
