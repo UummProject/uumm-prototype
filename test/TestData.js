794828e8f6ae37c71991a28bea2c15f6bc6a41ee
Xavi <xavings@gmail.com>
2017-08-11 12:11:18 +0200

    Adding contributors  votes into testdata

--- test/TestData.js
+++ test/TestData.js
@@ -12,31 +12,38 @@ exports.proposal1=
             state:0,
             positiveVotes:0,
             negativeVotes:0,
-            totalSupply:1
+            totalSupply:1,
+            contributorVotes:[0]//Represents the list of contributors
         },
         {
-            //#1 ProjectCreator (1token) voted to approve
+            //#1 ProjectCreator (1 token) voted to approve
             state:0,
             positiveVotes:1,
             negativeVotes:0,
-            totalSupply:1
+            totalSupply:1,
+            contributorVotes:[1]
         },
         {
-            //#2 ProjectCreator (1token) voted to approve(again). Data stays the same
+            //#2 ProjectCreator (1 token) voted to approve(again).
+            //Data stays the same
             state:0,
             positiveVotes:1,
             negativeVotes:0,
-            totalSupply:1
+            totalSupply:1,
+            contributorVotes:[1]
         },
         {
-            //#3 ProjectCreator (1token) voted against. Postive vote goes back to zero, negative to one
+            //#3 ProjectCreator (1 token) voted against. 
+            //Postive vote goes back to zero, negative to one
             state:0,
             positiveVotes:0,
             negativeVotes:1,
-            totalSupply:1
+            totalSupply:1,
+            contributorVotes:[-1]
         },
         {
-            //#4 ProjectCreator (1token) voted in favor. Negative vote goes back to zero, positive to  one
+            //#4 ProjectCreator (1 token) voted in favor.
+            //Negative vote goes back to zero, positive to  one
             state:0,
             positiveVotes:1,
             negativeVotes:0,
