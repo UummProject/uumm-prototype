7719f2eeb07efeced7678b7314b91d58d56c1e3b
Xavi <xavings@gmail.com>
2017-08-13 18:06:08 +0200

    Making unconfirmed proposal card looking like the proposal card

--- src/UnconfirmedProposalCard.js
+++ src/UnconfirmedProposalCard.js
@@ -1,5 +1,16 @@
 import React from 'react'
-import Paper from 'material-ui/Paper'
+import InProgressIcon from 'react-icons/lib/md/keyboard-control'
+
+const cardStyle =
+{
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    padding : 5,
+    margin:5
+}
 
 const containerStyle =
 {
@@ -8,15 +19,36 @@ const containerStyle =
     flexWrap: 'nowrap',
     justifyContent: 'space-between',
     alignItems: 'center',
-    height : 80,
-    padding : 15,
-    marginTop : 10,
-    margnBottom : 30,
-    marginLeft:5,
-    marginRight:5
+    width:"100%"
 }
 
-class UnconfimedProposalCard extends React.Component {
+const pStyle = 
+{
+    color:"#aaa"
+}
+
+const cellStyle = 
+{
+    flexGrow:1,
+    textAlign:"center",
+    flexShring:0
+}
+
+const stateCellStyle = 
+{
+    textAlign:"center",
+    flexGrow:1,
+    width:110,
+    color:"#ccc"
+}
+
+const emptyCellStyle = 
+{
+    flexGrow:1,
+    flexShring:1
+}
+
+class ProposalCard extends React.Component {
 
     constructor(props)
     {
@@ -24,14 +56,22 @@ class UnconfimedProposalCard extends React.Component {
     }
 
     render()
-    {  
+    {
         return (
-            <Paper style={containerStyle} zDepth={1} >
-                <h4 style={{"color":"#9E9E9E"}}> {this.props.proposalData.title} </h4> 
-                <p style={{"color":"#9E9E9E"}}> unconfirmed </p>
-            </Paper>
+            <div style={cardStyle} >
+                <h3 style={{color:"#ccc", margin:7}}> {this.props.proposalData.title} </h3> 
+                
+                 <div style={containerStyle} >
+
+                    <div style={cellStyle}/>
+                    <div style={cellStyle}/>
+                    <div style={cellStyle}> </div>
+                    <div style={cellStyle}> </div>
+                    <div style={cellStyle}><p style={pStyle}> ...unconfirmed </p></div>
+                </div>
+            </div>
         )
     }
 }
 
-export default UnconfimedProposalCard
+export default ProposalCard
