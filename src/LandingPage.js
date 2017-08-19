9408f2b428fbc1bcda1ac42654abbcdae7dc9849
Xavi <xavings@gmail.com>
2017-08-19 17:48:13 +0200

    The shorter the better

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -10,14 +10,12 @@ const whatIsThis=`
 In recent years a new paradigm around decentralization has emerged.
 Technologies such as [Ethereum](https://www.ethereum.org/), [IPFS](https://ipfs.io/) or [Bitcoin](https://en.wikipedia.org/wiki/Bitcoin) offer a new set of possibilities for re-designing most of the systems that drive our society.
 
-Uumm is a tool for the process of decision making
-
-For good decisions to be made the right incentives must be in place. One of the goals of this project is to create a tool that makes easy to create such ecosystem.
+Uumm is an open-source, experimental tool, for the process of decision making build on top of these new exciting technologies.
 
 # Where are we
 We have a first working prototype. On the [Github repo](https://github.com/xavivives/uumm) you can find all the details about it.
 
-At the same time we are already working on a second iteration.
+We're also working on the next second iteration.
 
 [Follow us on twitter](https://twitter.com/xavivives), to stay up to date.`
 
