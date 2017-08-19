796af99a93c6fe1285efc9143e00e14efaa1145f
Xavi <xavings@gmail.com>
2017-08-19 17:17:44 +0200

    Text

--- src/Page.js
+++ src/Page.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react'
 import Warning from './Warning.js'
+import RaisedButton from 'material-ui/RaisedButton'
+
+const FloatingButtonStyle = {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed',
+    zIndex:100
+};
+
+const FloatingButtonStyle2 = {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 60,
+    left: 'auto',
+    position: 'fixed',
+    zIndex:100
+};
+
+const twitterLink = 'http://twitter.com/home?status=@xavivives About the Uummm tool...'
+const issueLink = 'https://github.com/xavivives/uumm/issues/new'
 
 class Page extends Component
 {
@@ -11,10 +35,27 @@ class Page extends Component
                     <div style={{margin:20, maxWidth:600, minWidth:400}}> 
                     
                         <Warning>           
-                             <p>This project is in alpha stage. Don't use it for important stuff</p>        
+                             <p>This tool is highly experiemental and on an alpha stage still. Don't use it for important stuff</p>        
                          </Warning>
                          
                          {this.props.children}
+
+                         <RaisedButton
+                            style = {FloatingButtonStyle2}
+                            href = {twitterLink}
+                            secondary={false}
+                            disabled={false}
+                            label ='Feedback?'
+                            />
+
+                            <RaisedButton
+                            style = {FloatingButtonStyle}
+                            href = {issueLink}
+                            onTouchTap={this.onProblemClicked}
+                            secondary={false}
+                            disabled={false}
+                            label ='Problems?'
+                            />
 
                      </div>                             
                 </div>
