a473d4a0c71da13e3a7ae691922b80e73f5c3966
Xavi <xavings@gmail.com>
2017-08-20 23:12:10 +0200

    Updated 'deconstruction governance' and moved fewfiles into support

---
+++ support/Deconstructing governance.md
@@ -0,0 +1,42 @@
+# Interoperabile Adhocracy
+
+
+## The ideal governance
+For "ideal" I mean the necessary features that a system needs to make the best decisions possible, for whatever the goal is.
+
+### Meritocracy as a base.
+All the governance models are a supset of Meritocracy, if you understand _merit_ as the feature that defines the right to vote.
+
+For a meritocracy to work, the defniniton of merit needs to create incentives towards the system goals. It should tell what it gives value to. If is defined broadly or incorrectly the system is likely to [fail](http://lesswrong.com/lw/y3/value_is_fragile/) or be gammed.
+
+### Merit is not a currency
+In rerospective it seems so ovious, but it wasn't that clear before.
+
+`Merit can't be a medium of exchange`
+
+If you can trade the merit you end up with a system ruled by the entities that have more economic power.
+
+### Clear and simple goal
+It is required for merit to be defined accurately
+
+### Identity agnostic
+If the goal is to make the best decision possible, it should not matter where the ideas com from. Anyone/thing should be a potential participant, whatever it's a kid, an AI, a prediction market, another governance entity.
+
+### Nestable
+That means a governance system should be able to act as a participant entity to another governance system.
+
+Governance models tend to complexity. By nesting a governance inside another the incentives towards simple clear goals are kept, while adding sofistication to the system as a whole.
+
+
+
+### Interoperable
+If it is nestable, it needs to to communicate with other governaces (parents or childs). That means that some sort of interoperability should exist.
+
+###Friction less
+###Voluntary
+###Simple
+###Enforcable or turstable
+
+
+
+##What can we build
---
+++ support/DeconstructionGovernanceExamples.js
@@ -0,0 +1,72 @@
+{
+    name:'Small busineses',
+    decisions:['what pricing put', 'employees salaries']
+    participants:'Owner',
+    merit:'Put energy on understanding and fixing a problem',
+    metric:'Document telling the business is her',
+    incentives:
+}
+
+
+{
+    name:'Basketball team',
+    decisions:['What player plays', 'what moves']
+    participants:'Trainer', 'Point Guard',
+    merit:'Capacity to read the game', 'know the players',
+    metric:'Position','trust',
+    incentives:'Win the game', 'reputation', 'economic'
+}
+
+
+
+{
+    name:'Corporation',
+    decisions:['Overall direction']
+    participants:'Shareholders', 'board of directors',
+    merit:'Own company shares',
+    metric:'Contracts',
+    incentives:'Increase company valuation'
+}
+
+{
+    name:'Engineering department',
+    decisions:['How to implement somethiong', 'employees salaries']
+    participants:'Head of engineering, employees'
+    merit:['pervious experience', 'fidelity to the company', 'academic titles']
+    metric:'Contract',
+    incentives:['reputation', 'payout', 'economic']
+}
+
+{
+    name:'Futarchy',
+    decisions:['any']
+    merit:'Forecast futur events',
+}
+
+{
+    name:'Dictatorship',
+    decisions:['any']
+    merit:'Control military',
+    incentives:['Keep control','economic']
+}
+
+{
+    name:'Futarchy',
+    decisions:['any']
+    merit:'Forecast futur events correctly',
+    metric:'Number of certain predictions'
+}
+
+{
+    name:'Bitcoin',
+    decisions:['change the protocol', 'validate transaction']
+    merit:['validate transactions','secure the network']
+    metric:'hashing power',
+    incentives:'award bitcoins to themselves', 'economic'
+}
+
+participants:'Who can make decisions?'
+vote:'How is the decision defined?'
+merit:'What gives an entity the right to vote?'
+metric:'How to quantify the merit'
+incentives:'Why will the entity make good decisions?'
