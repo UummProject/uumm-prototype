39ee007386a1ffca6057c74bf2f60e4a7de0ce98
Xavi <xavings@gmail.com>
2017-09-01 10:03:09 +0200

    Spacing

--- src/ProposalsList.js
+++ src/ProposalsList.js
@@ -66,7 +66,6 @@ class ProposalsList extends React.Component
                     />);
 
                 proposalsComponents.push(<Divider key={"d"+ proposalData.id}/>)
-
             }
         }
 
