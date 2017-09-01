f05c046efdebd7bb0003364d69ab1c690a69d44c
Xavi <xavings@gmail.com>
2017-09-01 13:31:48 +0200

    Handling voted state

--- src/ProposalCard.js
+++ src/ProposalCard.js
@@ -5,7 +5,7 @@ import ApproveIcon from 'react-icons/lib/fa/check'
 import DenniedIcon from 'react-icons/lib/fa/close'
 import InProgressIcon from 'react-icons/lib/md/keyboard-control'
 import Numeral from 'numeral'
-import ProposalChart from './ProposalChart.js'
+
 
 const cardStyle =
 {
@@ -18,15 +18,22 @@ const cardStyle =
     paddingBottom:20,
 }
 
-const containerStyle =
+const bodyRowStyle =
 {
     padding:20,
     display: 'flex',
     flexDirection: 'row',
-    flexWrap: 'nowrap',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width:"100%"
+    //flexWrap: 'nowrap',
+    justifyContent: 'flex-start',
+   // alignItems: 'left',
+    //width:"100%"
+}
+
+const bodyColumnStyle=
+{
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'left',
 }
 
 const headerContainerStyle =
@@ -97,25 +104,49 @@ class ProposalCard extends React.Component {
        this.setState({extended:!this.state.extended})
     }
 
-    getAction =(state, hasConcensus, isOwner)=>
+    getUserState =(state, hasConcensus, isOwner, vote)=>
     {
         switch (state)
         {
             case State.ProposalState.PENDING:
-                if(hasConcensus)
-                {
-                    return (<RaisedButton label={"Resolve"} onTouchTap={this.onResolve}/>)
-                }
-                else
-                {
-                    if(!isOwner)
-                        return (<p> </p>)
 
-                    return (<div>
+                    if(vote === 0)
+                    {
+                        return (<div>
                             <RaisedButton style={{minWidth:50, width:50}} icon={<ApproveIcon/>} onTouchTap={this.onPositiveVote}/>
                             <RaisedButton style={{minWidth:50, width:50}} icon={<DenniedIcon/>} onTouchTap={this.onNegativeVote}/>
                         </div>)
-                }
+                    }
+
+                    let voted = <div/>
+                    let resolve = <div/>
+                    if (vote > 0)
+                    {
+                        voted = (<div>
+                                    <ApproveIcon  size={25}/>
+                                    <p> {Math.abs(vote)}</p>
+                            </div>)
+                    }
+                    else if (vote < 0)
+                    {
+                        voted = (<div>
+                                    <DenniedIcon  size={25}/>
+                                    <p> {Math.abs(vote)}</p>
+                            </div>)
+                    }
+
+
+                    if(hasConcensus)
+                    {
+                        resolve = (<RaisedButton label={"Resolve"} onTouchTap={this.onResolve}/>)
+                    }
+
+                    return (<div>
+                            {voted}
+                            {resolve}
+                        </div>)
+                    
+                
             case State.ProposalState.APPROVED: return (<ApproveIcon  size={25}/>)
             case State.ProposalState.DENIED: return (<DenniedIcon size={25}/>)
             case State.ProposalState.EXPIRED: return (<p> Expired </p>)
@@ -143,40 +174,68 @@ class ProposalCard extends React.Component {
     render()
     {  
         //TODO: Most of this data should come from props
-        var contributorData = State.getContributorData(this.props.projectId, this.props.userAddress)
-        var isOwner = false
-        if(contributorData)
-            isOwner = contributorData.valueTokens > 0
 
-        var totalSupply = this.props.projectData.totalSupply
+        //State data
+        let totalSupply = this.props.projectData.totalSupply
         if(this.props.proposalData.state !== State.ProposalState.PENDING)
             totalSupply = this.props.proposalData.totalSupply
 
-        var positiveVotes = this.props.proposalData.positiveVotes / totalSupply
-        var negativeVotes = this.props.proposalData.negativeVotes / totalSupply
-        //var participation = positiveVotes + negativeVotes
-        var hasConcensus = (positiveVotes > this.props.projectData.requiredConcensus) || (negativeVotes > this.props.projectData.requiredConcensus)
-        //var hasEnoughParticipation =  participation > this.props.projectData.requiredParticipation
+        let positivePercentage = this.props.proposalData.positiveVotes / totalSupply
+        let negativePercentage = this.props.proposalData.negativeVotes / totalSupply
+        //let participation = positivePercentage + negativePercentage
+        let hasConcensus = (positivePercentage > this.props.projectData.requiredConcensus) || (negativePercentage > this.props.projectData.requiredConcensus)
+        //let hasEnoughParticipation =  participation > this.props.projectData.requiredParticipation
 
-        var actions = this.getAction(this.props.proposalData.state, hasConcensus, isOwner) 
-        var stateIcon = this.getStateIcon(this.props.proposalData.state, hasConcensus, isOwner) 
 
-        var header = (<div style = {headerContainerStyle} onClick={this.onTitleClicked} >
+        //User data
+        let contributorData = State.getContributorData(this.props.projectId, this.props.userAddress)
+        let contributorVote = State.getContributorVote(this.props.projectId, this.props.proposalData.id, this.props.userAddress)
+        let userState = this.getUserState(this.props.proposalData.state, hasConcensus, isOwner, contributorVote) 
+            
+
+
+
+        let isOwner = false
+        if(contributorData)
+            isOwner = contributorData.valueTokens > 0
+
+        
+        let stateIcon = this.getStateIcon(this.props.proposalData.state, hasConcensus, isOwner) 
+
+
+
+        let header = (<div style = {headerContainerStyle} onClick={this.onTitleClicked} >
                         
                         <h3 style={{margin:5}}> {this.props.proposalData.title} </h3> 
                         {stateIcon}
                     </div>)
 
-        var body = <div/>
+        let body = <div/>
 
         if (this.state.extended)
-            body =(<div style={containerStyle}>
-                        <div style={stateCellStyle}> {actions} </div>
-                        <div style={emptyCellStyle}/>
-                        <div style={cellStyle}><p style={pStyle}> Tokens asked: {this.props.proposalData.valueAmount} </p> </div>
-                        <div style={cellStyle}><p style={pStyle}> <ApproveIcon/> {Numeral(positiveVotes).format('0.0%')} </p></div>
-                        <div style={cellStyle}><p style={pStyle}> <DenniedIcon/> {Numeral(negativeVotes).format('0.0%')} </p></div>
-                        <ProposalChart size ={100} negativeVotes={negativeVotes} positiveVotes={positiveVotes} total = {totalSupply} />
+            body =(<div>
+                        <div style={bodyRowStyle}>
+                            <div style={bodyColumnStyle}>
+                                <p style={pStyle}> Proposal state: </p>
+                                <div ><p style={pStyle}> <ApproveIcon/> {Numeral(positivePercentage).format('0.0%')}, {this.props.proposalData.positiveVotes} votes</p></div>
+                                <div ><p style={pStyle}> <DenniedIcon/> {Numeral(negativePercentage).format('0.0%')}, {this.props.proposalData.negativeVotes} votes</p></div>
+                            </div>
+                        </div>
+
+                        <div style={bodyRowStyle}>
+                            <div style={bodyColumnStyle}>
+                                <p style={pStyle}> Your vote: </p>
+                                <div style={stateCellStyle}> {userState} </div>
+                            </div>
+                        </div>
+
+                        <div style={bodyRowStyle}>
+                            <div style={bodyColumnStyle}>
+                                <p style={pStyle}> Tokens asked: {this.props.proposalData.valueAmount} </p>
+                                <p style={pStyle}> Description: [Sorry, we're working on that] </p>
+                            </div>
+                        </div>
+
                     </div>)
 
         return (
